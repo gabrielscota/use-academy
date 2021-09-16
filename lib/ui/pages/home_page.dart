d63cf458d46fb9d263312c727cf14a97e68fb3f1
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-16 20:55:08 -0300

    feat: using fab and buttons

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,31 +6,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        splashColor: Colors.red[400],
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: SafeArea(
         child: SizedBox.expand(
           child: Container(
-            color: Colors.blue,
+            color: Colors.white,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.green,
+                ElevatedButton(
+                  onPressed: () => debugPrint('ElevatedButton onPressed'),
+                  onLongPress: () => debugPrint('ElevatedButton onLongPressed'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    onPrimary: Colors.green,
+                    padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
                   ),
+                  child: const Text('ElevatedButton'),
                 ),
-                Flexible(
-                  flex: 1,
-                  fit: FlexFit.tight,
-                  child: Container(
-                    color: Colors.amber,
-                    child: const Text('Teste'),
-                  ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('OutlinedButton'),
                 ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    color: Colors.red,
-                  ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('TextButton'),
                 ),
               ],
             ),
