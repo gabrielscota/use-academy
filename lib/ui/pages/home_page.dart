af1bb1a516cab7fdbf6956cd9a8ddb82c173a2ad
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-16 21:11:40 -0300

    feat: using GridView

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,42 +6,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        splashColor: Colors.red[400],
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.red,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: SafeArea(
         child: SizedBox.expand(
           child: Container(
             color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () => debugPrint('ElevatedButton onPressed'),
-                  onLongPress: () => debugPrint('ElevatedButton onLongPressed'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    onPrimary: Colors.green,
-                    padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                  ),
-                  child: const Text('ElevatedButton'),
-                ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text('OutlinedButton'),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('TextButton'),
-                ),
-              ],
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+              ),
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) => Container(
+                color: Colors.lightGreen,
+              ),
             ),
           ),
         ),
