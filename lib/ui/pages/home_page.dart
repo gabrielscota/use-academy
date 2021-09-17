8261f98ef35a1c7a8ca94e18044623d275a7c9a3
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-16 21:55:55 -0300

    feat: using Wrap

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -7,33 +7,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          height: 400,
-          child: ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            reverse: true,
-            scrollDirection: Axis.horizontal,
-            separatorBuilder: (context, index) => const SizedBox(height: 16.0, width: 16.0),
-            itemBuilder: (context, index) {
-              final String text = 'Item ${index + 1}';
-              final String date = DateTime.now().day.toString();
-
-              return Container(
-                color: Colors.blueGrey,
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                height: 200,
-                alignment: Alignment.center,
-                child: Text(
-                  '$text - Dia: $date',
-                  style: const TextStyle(
-                    fontSize: 32.0,
-                    color: Colors.white,
-                  ),
+        child: SizedBox.expand(
+          child: Container(
+            color: Colors.white,
+            child: Wrap(
+              spacing: 16.0,
+              alignment: WrapAlignment.center,
+              children: [
+                Chip(
+                  label: const Text('Teste Wrap with Chip'),
+                  deleteIcon: const Icon(Icons.close_rounded),
+                  onDeleted: () {},
                 ),
-              );
-            },
-            itemCount: 10,
+                Chip(
+                  label: const Text('Teste Wrap'),
+                  deleteIcon: const Icon(Icons.close_rounded),
+                  onDeleted: () {},
+                ),
+                Chip(
+                  label: const Text('Teste Wrap with Chip'),
+                  deleteIcon: const Icon(Icons.close_rounded),
+                  onDeleted: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
