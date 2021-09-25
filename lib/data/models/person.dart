128c9d0e2f589e0c93716e1070df27b22da4d641
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-24 22:22:10 -0300

    feat: data layer with remote model

---
+++ lib/data/models/person.dart
@@ -0,0 +1,23 @@
+class RemotePersonModel {
+  final String id;
+  final String name;
+  final int age;
+  final int height;
+  final int weight;
+
+  RemotePersonModel({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+    required this.weight,
+  });
+
+  factory RemotePersonModel.fromJson(Map<String, dynamic> json) => RemotePersonModel(
+        id: (json['id'] ?? '').toString(),
+        name: (json['name'] ?? '').toString(),
+        age: (json['age'] ?? 0) as int,
+        height: (json['height'] ?? 0) as int,
+        weight: (json['weight'] ?? 0) as int,
+      );
+}
