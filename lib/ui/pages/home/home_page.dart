a394ad1e9007f3bac6105004de3fa3c24ac68e67
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-24 22:21:19 -0300

    feat: modified home page to use stream

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,192 +1,63 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/models/models.dart';
 import '../pages.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  final HomePresenter presenter;
+
+  const HomePage({Key? key, required this.presenter}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    widget.presenter.loadPersons();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SizedBox.expand(
-          child: Column(
-            children: const [
-              FirstContainer(),
-              SecondContainer(),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: StreamBuilder<List<RemotePersonModel>>(
+                    stream: widget.presenter.peopleStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                        return ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: snapshot.data?.length,
+                          itemBuilder: (context, index) => ListTile(
+                            title: Text(snapshot.data![index].name),
+                            subtitle: Text(snapshot.data![index].age.toString()),
+                          ),
+                        );
+                      } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+                        return const Center(child: Text('Nenhum resultado encontrado!'));
+                      } else {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: widget.presenter.loadPersons,
+                  child: const Text('Carregar pessoas'),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondContainer extends StatefulWidget {
-  const SecondContainer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<SecondContainer> createState() => _SecondContainerState();
-}
-
-class NavigationArguments {
-  final String routeName;
-  final Object? arguments;
-
-  NavigationArguments({required this.routeName, this.arguments});
-}
-
-class ProfileNavigationArguments {
-  final String name;
-  final int age;
-  final double height;
-
-  ProfileNavigationArguments({required this.name, required this.age, required this.height});
-}
-
-class _SecondContainerState extends State<SecondContainer> {
-  late String text;
-  late String name;
-  late int age;
-  late double height;
-
-  @override
-  void initState() {
-    text = 'Profile Page';
-    name = 'Gabriel';
-    age = 21;
-    height = 1.75;
-
-    super.initState();
-  }
-
-  void goToProfilePage() {
-    final ProfileNavigationArguments profileArguments = ProfileNavigationArguments(
-      name: name,
-      age: age,
-      height: height,
-    );
-
-    final NavigationArguments arguments = NavigationArguments(
-      routeName: '/profile',
-      arguments: profileArguments,
-    );
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const ProfilePage(),
-        settings: RouteSettings(
-          name: '/profile',
-          arguments: arguments,
-        ),
-      ),
-    );
-  }
-
-  Color borderColor = Colors.blue;
-  Color backgroundColor = Colors.white;
-  Widget checkboxContent = const SizedBox();
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.green[100],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Nome: $name'),
-            Text('Idade: $age'),
-            const SizedBox(height: 16.0),
-            Container(
-              height: 32.0,
-              width: 32.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(
-                  color: borderColor,
-                  width: 2.0,
-                ),
-                color: Theme.of(context).primaryColor,
-              ),
-              child: checkboxContent,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  text,
-                  style: Theme.of(context).textTheme.headline5?.copyWith(
-                        color: Colors.blue,
-                      ),
-                ),
-                const SizedBox(width: 16.0),
-                ElevatedButton(
-                  onPressed: goToProfilePage,
-                  child: const Text('Ir para a tela'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class FirstContainer extends StatelessWidget {
-  const FirstContainer({
-    Key? key,
-  }) : super(key: key);
-
-  final String text = 'Profile Page';
-  final String name = 'Gabriel';
-  final int age = 21;
-
-  void goToProfilePage(BuildContext context) {
-    final Map<String, dynamic> arguments = {
-      "name": name,
-      "age": age,
-    };
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const ProfilePage(),
-        settings: RouteSettings(
-          name: '/profile',
-          arguments: arguments,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.blue[100],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Nome: $name'),
-            Text('Idade: $age'),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(text),
-                const SizedBox(width: 16.0),
-                ElevatedButton(
-                  onPressed: () => goToProfilePage(context),
-                  child: const Text('Ir para a tela'),
-                ),
-              ],
-            ),
-          ],
         ),
       ),
     );
