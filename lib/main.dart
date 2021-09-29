eeaa5d9215f08e10b69fc33c2a2e7672ec73b0f8
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-28 21:43:05 -0300

    feat: configure and initialize crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
@@ -13,6 +15,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  }
 
   runApp(MyApp());
 }
