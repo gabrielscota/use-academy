df8bd3a3aeca27ab5368aea5fb697aee73ccfa46
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-09-29 20:33:23 -0300

    feat: getx login presenter

---
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+import '../../ui/pages/pages.dart';
+
+class GetxLoginPresenter extends GetxController implements LoginPresenter {
+  String _email = '';
+  String _password = '';
+
+  @override
+  Future<void> auth() async {}
+}
--- lib/presentation/presenters/presenters.dart
+++ lib/presentation/presenters/presenters.dart
@@ -1,2 +1,3 @@
 export 'getx_home_presenter.dart';
+export 'getx_login_presenter.dart';
 export 'stream_profile_presenter.dart';
